520ab64d047963acedb18cb529691466e94474e3
victor-schumann <schumann@tutamail.com>
2023-01-30 19:12:53 +0000

    Basics & set-up

---
+++ my-first-app/src/app/server/server.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    //information that tells angular what to do with this class
+    selector: 'app-server',
+    templateUrl: ''
+})
+export class ServerComponent {
+
+}
---
+++ ts/basics.ts
@@ -0,0 +1,22 @@
+//primitives
+let age: number;
+let userName: string;
+let isStudent: boolean;
+
+age = 19;
+userName = "Yu";
+isStudent = true;
+
+//complex
+let hobbies: string[];
+type Person = {
+    name: string;
+    age: number;
+};
+let person: Person;
+
+hobbies = ['videogames', 'reading'];
+person = {
+    name: 'Max',
+    age: 19
+}
