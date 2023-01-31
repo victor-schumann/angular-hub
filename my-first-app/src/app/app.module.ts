552467ba5b58219263214900f74dd8e122f09425
victor-schumann <schumann@tutamail.com>
2023-01-31 22:13:11 +0000

    Set up server1 & servers components

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ServerComponentOne } from './server1/server1.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ServerComponentOne,
+    ServersComponent
   ],
   imports: [
     BrowserModule,
---
+++ my-first-app/src/app/server1/server1.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component( {
+    selector: '<app-server1',
+    templateUrl: './server1.component.html'
+})
+
+export class ServerComponentOne  {
+
+}
