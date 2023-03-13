a9d95965526769289835eab385a6c2e82200f5ba
victor-schumann <schumann@tutamail.com>
2023-03-13 22:48:00 +0000

    [NINC-1235][NINC-1970] Fix bad service import

--- 10-services/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ 10-services/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -7,6 +7,7 @@ import {
 
 import { Ingredient } from '../../shared/ingredient.model';
 import { ShoppingListComponent } from '../shopping-list.component';
+import { ShoppingListService } from '../shopping-list.service';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -17,7 +18,7 @@ export class ShoppingEditComponent implements OnInit {
   @ViewChild('nameInput', { static: false }) nameInputRef!: ElementRef;
   @ViewChild('amountInput', { static: false }) amountInputRef!: ElementRef;
 
-  constructor(private slService: ShoppingListComponent) { }
+  constructor(private slService: ShoppingListService) { }
 
   ngOnInit() {
   }
@@ -26,7 +27,6 @@ export class ShoppingEditComponent implements OnInit {
     const ingName = this.nameInputRef.nativeElement.value;
     const ingAmount = this.amountInputRef.nativeElement.value;
     const newIngredient = new Ingredient(ingName, ingAmount);
-
     this.slService.addIngredient(newIngredient);
   }
 
