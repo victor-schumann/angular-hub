278fcea1a518f9cb5024bb26dc40ff066e906083
victor-schumann <schumann@tutamail.com>
2023-05-14 16:11:54 +0100

    [NINC-1235][NINC-1969] #221-2

--- 16-prj-forms/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ 16-prj-forms/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,22 +1,38 @@
 import {
   Component,
+  OnDestroy,
   OnInit
 } from '@angular/core';
 
 import { Ingredient } from '../../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
 import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
-export class ShoppingEditComponent implements OnInit {
+export class ShoppingEditComponent implements OnInit, OnDestroy {
+  subscription: Subscription
+  editMode = false;
+  editedItemIndex: number;
 
   constructor(private slService: ShoppingListService) { }
 
   ngOnInit() {
+    this. subscription = this.slService.startedEditing
+    .subscribe(
+      (index: number) => {
+        this.editMode = true;
+        this.editedItemIndex = index;
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
   onAddItem(form: NgForm) {
