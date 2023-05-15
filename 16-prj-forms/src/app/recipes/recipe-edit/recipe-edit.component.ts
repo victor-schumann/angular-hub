42c56eec3e3eb18971c152cdcfeedddfaddda59b
victor-schumann <schumann@tutamail.com>
2023-05-15 22:49:45 +0100

    [NINC-1235][NINC-1969] #235

--- 16-prj-forms/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ 16-prj-forms/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { RecipeService } from '../recipe.service';
-import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -16,7 +15,8 @@ export class RecipeEditComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
-    private recipeService: RecipeService
+    private recipeService: RecipeService,
+    private router: Router
   ) {}
 
   get controls() {
@@ -46,6 +46,8 @@ export class RecipeEditComponent implements OnInit {
     } else {
       this.recipeService.addRecipe(this.recipeForm.value);
     }
+
+    this.onCancel();
   }
 
   private initForm(): void {
@@ -94,5 +96,9 @@ export class RecipeEditComponent implements OnInit {
         ]),
       })
     );
+  }
+
+  onCancel() {
+    this.router.navigate(['../'], { relativeTo: this.route});
   }
 }
